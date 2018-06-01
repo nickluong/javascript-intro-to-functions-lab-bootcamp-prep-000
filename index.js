8ed25ecd8fe690c9ba4f1c0b3bc40a7599f45011
Nicholas  Luong <nckluong@gmail.com>
2018-06-01 01:16:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var lowercase = string.whisper()
+  var lowercase = whisper(string)
   if(string === lowercase) return "I can't hear you!"
-  var uppercase = string.shout()
+  var uppercase = shout(string)
   if(string === uppercase) return "YES INDEED!"
   var love = "I love you, Grandma."
   if(string === love) return "I love you, too."
